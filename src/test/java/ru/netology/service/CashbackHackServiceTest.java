e494d820fe2fd9ce946a2a5c6ee26f87b35669ea
Lenar Khafizov <lenarkhaf@gmail.com>
2025-05-03 21:17:37 +0300

    Исправлена ошибка в тестах и в коде программы (тесты с использованием testng)

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,9 +8,9 @@ public class CashbackHackServiceTest {
     @Test
     void shouldCashbackServiceAmountZero(){
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(0);
-        int expected = 0;
-        assertEquals(expected,actual);
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.remain(0);
+        });
     }
 
     @Test
