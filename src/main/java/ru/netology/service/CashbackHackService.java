ff3982a338169649477de56b3b12d19e22333343
Lenar Khafizov <lenarkhaf@gmail.com>
2025-05-06 21:58:24 +0300

    Класс  CashbackHackService приведен к первоначальному коду (в Initial commit)

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,10 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Сумма покупки должна быть положительной, а у Вас: "
-                    + amount);
-        }
         return boundary - amount % boundary;
     }
 }
